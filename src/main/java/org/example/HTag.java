285721c31074a85cf1c79118cf20f0660be0731d
sebbex1337 <sefort@hotmail.dk>
2024-02-01 09:05:28 +0100

    Initial commit

---
+++ src/main/java/org/example/HTag.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class HTag extends Tag {
+    private byte number;
+
+    public HTag(int hnum) {
+        this.number = (byte)hnum;
+        this.setTagName("h" + number);
+    }
+
+    public byte getNumber() {
+        return number;
+    }
+
+    public void setNumber(byte number) {
+        this.number = number;
+    }
+    public void setNumber(int number) {
+        this.number = (byte) number;
+    }
+
+    @Override
+    public String toString() {
+        String s = super.toString();
+        return s + "HTag{" +
+                "number=" + number +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        HTag hTag = new HTag(1);
+        hTag.setText("jeg er et h tag");
+
+        System.out.println(hTag);
+    }
+}
